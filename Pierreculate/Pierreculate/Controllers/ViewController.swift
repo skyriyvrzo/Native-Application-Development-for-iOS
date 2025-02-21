a0e9036a8346e0e1b1e2154375b0460af3b4b8ca
GE77 <skyriyvrzo@gmail.com>
2025-02-21 12:17:49 +0700

    2025.2.21.2

---
+++ Pierreculate/Pierreculate/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pierreculate
+//
+//  Created by MII-MAC-20 on 21/2/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
